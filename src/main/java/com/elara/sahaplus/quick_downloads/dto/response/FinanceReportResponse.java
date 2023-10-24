9f5e66d7fe6a988d58c497bb18d035b96d915e4f
Paul Akerejola <96719801+DevJola@users.noreply.github.com>
2023-10-24 05:55:55 +0100

    implement endpoints to download various finance reports

---
+++ src/main/java/com/elara/sahaplus/quick_downloads/dto/response/FinanceReportResponse.java
@@ -0,0 +1,56 @@
+package com.elara.sahaplus.quick_downloads.dto.response;
+
+import com.elara.sahaplus.util.BaseResponse;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class FinanceReportResponse extends BaseResponse {
+    private Data data;
+
+    public FinanceReportResponse() {
+        super();
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Data {
+        private String institutionName;
+        private String code;
+        private String institutionStatus;
+        private LocalDateTime billingMonth;
+        private BigDecimal totalPaid;
+        private BigDecimal outStanding;
+        private BigDecimal subscriptionCharged;
+        private BigDecimal totalSubscriptionPaid;
+        private BigDecimal mobileTellerSubscriptionCharged;
+        private BigDecimal sMSPaid;
+        private BigDecimal virtualAirtimePaid;
+        private BigDecimal branchless;
+        private BigDecimal cards;
+        private BigDecimal inwardRemittance;
+        private BigDecimal correspBanking;
+        private BigDecimal selfService;
+        private BigDecimal bvn;
+        private BigDecimal email;
+        private BigDecimal xds;
+        private BigDecimal channelAccessFeeDifferential;
+        private BigDecimal interBankFee;
+        private BigDecimal totalAppzoneChannelAccessCharge;
+        private BigDecimal channelAccessFeePaid;
+        private BigDecimal outstandingBalanceEnquiryCharge;
+        private BigDecimal totalChannelAccessCharge;
+        private int usageDate;
+        private LocalDateTime minimumUsageDate;
+        private BigDecimal miscReceivableForTheMonth;
+        private BigDecimal receivablesFromUserSubscription;
+    }
+}
