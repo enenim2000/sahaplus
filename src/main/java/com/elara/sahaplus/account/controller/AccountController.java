2d3b683519c7e64d63291cae62fbfede90bb2a01
Paul Akerejola <96719801+DevJola@users.noreply.github.com>
2023-10-27 13:54:04 +0100

    Implement endpoint to close account

--- src/main/java/com/elara/sahaplus/account/controller/AccountController.java
+++ src/main/java/com/elara/sahaplus/account/controller/AccountController.java
@@ -1,6 +1,8 @@
 package com.elara.sahaplus.account.controller;
 
+import com.elara.sahaplus.account.dto.request.CloseAccountRequest;
 import com.elara.sahaplus.account.dto.request.CreateCustomerAndAccountRequest;
+import com.elara.sahaplus.account.dto.response.CloseAccountResponse;
 import com.elara.sahaplus.account.dto.response.CreateCustomerAndAccountResponse;
 import com.elara.sahaplus.account.service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.InvocationTargetException;
 
 @RestController
 @Tag(name = "Account Module", description = "Account Module")
@@ -34,6 +38,20 @@ public class AccountController {
   @PostMapping("/CreateCustomerAndAccount")
   public CreateCustomerAndAccountResponse createCustomerAndAccount(@Valid @RequestBody CreateCustomerAndAccountRequest request) {
     return accountService.createCustomerAndAccount(request);
+  }
+
+  @Operation(summary = "Close Account")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Close Account",
+                  content = {@Content(mediaType = "application/json",
+                          schema = @Schema(implementation = CreateCustomerAndAccountResponse.class))})})
+  @PostMapping("/closeAccount")
+  public CloseAccountResponse closeAccount(
+          @Valid
+          @RequestBody CloseAccountRequest request)
+          throws InvocationTargetException, IllegalAccessException
+  {
+    return accountService.closeAccount(request);
   }
 }
 
---
+++ src/main/java/com/elara/sahaplus/account/dto/backbone/CloseAccountResponseDto.java
@@ -0,0 +1,23 @@
+package com.elara.sahaplus.account.dto.backbone;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CloseAccountResponseDto {
+    @JsonProperty("IsSuccessful")
+    private boolean isSuccessful;
+
+    @JsonProperty("Message")
+    private String message;
+
+    @JsonProperty("TransactionTrackingRef")
+    private String transactionTrackingRef;
+
+    @JsonProperty("CustomerIDInString")
+    private String customerIDInString;
+}
