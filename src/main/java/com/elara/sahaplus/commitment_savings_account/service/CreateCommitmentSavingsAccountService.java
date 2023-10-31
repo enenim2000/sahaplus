6baf73ec39f60a8d567509fd6ef9eb13b5b07ab3
Asukwo Enenim <Asukwo.Enenim@Interswitchng.com>
2023-10-31 08:20:22 +0100

    implemented sanef module

--- src/main/java/com/elara/sahaplus/commitment_savings_account/service/CreateCommitmentSavingsAccountService.java
+++ src/main/java/com/elara/sahaplus/commitment_savings_account/service/CreateCommitmentSavingsAccountService.java
@@ -1,5 +1,8 @@
 package com.elara.sahaplus.commitment_savings_account.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CreateCommitmentSavingsAccountService {
 
 }
